cc473d05e52119a0ece11f243833de5a8c3e6a4f
kemley76 <kaden.emley@gmail.com>
2025-11-18 21:03:11 -0500

    create basic SSE + crawler structure
    Signed-off-by: kemley76 <kaden.emley@gmail.com>

--- backend/crawler/crawler.go
+++ backend/crawler/crawler.go
@@ -1,38 +1,132 @@
 package crawler
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	u "net/url"
+	"sync"
+	"time"
+)
+
+const WAITTIME = time.Second * 2
 
 type crawler struct {
-	seedURLs    []string
-	rw          http.ResponseWriter // Used to send responses to the client
-	client      http.Client         // Used to fetch data from the web pages its crawling
-	dataChannel chan pageData       // Used to forward data from the async crawling functions to the client
-	visitedURLs []string
-	domainData  map[string]domainData
+	seedURLs        []string
+	rw              http.ResponseWriter // Used to send responses to the client
+	client          *http.Client        // Used to fetch data from the web pages its crawling
+	dataChannel     chan pageData       // Used to forward data from the async crawling functions to the client
+	visitedURLs     []string
+	domainData      map[string]domainData
+	queues          map[string][]string // maps domains to URLs
+	queueLock       sync.Mutex
+	connectionAlive bool
+	wg              sync.WaitGroup
 }
 
 // NewCrawler creates a new crawler that will start from the given seed URLs
 func NewCrawler(rw http.ResponseWriter, seedURLs []string) *crawler {
 	return &crawler{
-		seedURLs: seedURLs,
-		rw:       rw,
-		// TODO: create http client
-		dataChannel: make(chan pageData),
-		domainData:  make(map[string]domainData),
+		seedURLs:        seedURLs,
+		rw:              rw,
+		client:          &http.Client{},
+		dataChannel:     make(chan pageData),
+		domainData:      make(map[string]domainData),
+		queues:          make(map[string][]string), // maps domains to URLs
+		connectionAlive: true,
 	}
 }
 
 // Crawl will start crawling from the seed URLs up to the provided depth
-func (c *crawler) Crawl(depth int) {
+func (c *crawler) Crawl(depth int, clientDone <-chan struct{}) {
+	for _, url := range c.seedURLs {
+		c.queuePage(url)
+	}
 
+	go func() {
+		rc := http.NewResponseController(c.rw)
+		for {
+			select {
+			case <-clientDone:
+				c.connectionAlive = false
+				fmt.Println("Connection closed")
+				return
+			case data := <-c.dataChannel:
+				fmt.Println("Sending data: ", data)
+				err := json.NewEncoder(c.rw).Encode(data)
+				c.rw.Write([]byte("\n\n"))
+				if err != nil {
+					fmt.Println("Error encoding data:", data)
+					return
+				}
+				rc.Flush()
+			}
+		}
+	}()
+
+	c.wg.Wait()
+}
+
+func (c *crawler) queuePage(rawURL string) {
+	c.wg.Add(1)
+	url, err := u.Parse(rawURL)
+	if err != nil {
+		panic("Invalid URL") // TODO: send error to user?
+	}
+	if url.Host == "" {
+		url, err = u.Parse("http://" + rawURL)
+	}
+
+	c.queueLock.Lock()
+	defer c.queueLock.Unlock()
+
+	q, ok := c.queues[url.Host]
+	if ok {
+		c.queues[url.Host] = append(q, rawURL)
+	} else {
+		c.queues[url.Host] = []string{rawURL}
+		go c.CrawlHost(url.Host)
+	}
 }
 
 // CrawlPage will crawl an individual result
-func (c *crawler) CrawlPage(url string) {
+func (c *crawler) CrawlHost(hostname string) {
+	fmt.Println("Crawling host: ", hostname)
+	for c.crawlNextPage(hostname) {
+		time.Sleep(WAITTIME)
 
+		if !c.connectionAlive {
+			break
+		}
+	}
 }
 
-// sendDataToClient will send the provided pageData to the client via an SSE
-func (c *crawler) sendDataToClient(data pageData) {
+// returns true if a page was found in the queue
+func (c *crawler) crawlNextPage(hostname string) bool {
+	c.queueLock.Lock()
+	defer c.queueLock.Unlock()
 
+	q, ok := c.queues[hostname]
+	if !ok {
+		return false
+	} else if len(q) == 0 {
+		delete(c.queues, hostname)
+		return false
+	}
+
+	go c.crawlPage(q[0])
+	c.queues[hostname] = q[1:]
+	return true
+}
+
+func (c *crawler) crawlPage(url string) {
+	defer c.wg.Done()
+	println("Crawling page...", url)
+	c.dataChannel <- pageData{
+		URL:          url,
+		Title:        "blah",
+		ResponseTime: 0,
+		Links:        []string{},
+		Errors:       []string{},
+	}
 }
--- backend/web/router.go
+++ backend/web/router.go
@@ -1,20 +1,40 @@
 package web
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/kemley76/web-crawl-vis/v2/crawler"
 )
 
 func RunServer() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	fs := http.FileServer(http.Dir("../public"))
-	r.Handle("/", fs)
+	r.HandleFunc("/crawl", crawl)
+	r.Handle("/*", http.FileServer(http.Dir("../public/")))
 
 	fmt.Println("Server running at localhost:3000")
 	http.ListenAndServe(":3000", r)
+}
+
+type CrawlRequest struct {
+	URLs []string `json:"urls"`
+}
+
+func crawl(rw http.ResponseWriter, r *http.Request) {
+	var crawlRequest CrawlRequest
+	err := json.NewDecoder(r.Body).Decode(&crawlRequest)
+	if err != nil {
+		fmt.Fprintf(rw, "Error parsing request: %v", err)
+	}
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+
+	crawler := crawler.NewCrawler(rw, crawlRequest.URLs)
+	crawler.Crawl(2, r.Context().Done())
 }
